246a71b793eae02180bb2e69cca1f1c2abc8e319
Carvajal Delgado, Mauricio <mauricio.carvajal.delgado@intel.com>
2019-08-20 20:38:56 -0600

    Stil working, pending to do the prediction

--- 2_Predicting_Customer_Preferences/2_Customer/Classification_Predict/Predict_which_Brand_of_Products_Customers_Prefer.R
+++ 2_Predicting_Customer_Preferences/2_Customer/Classification_Predict/Predict_which_Brand_of_Products_Customers_Prefer.R
@@ -1,9 +1,14 @@
 #Mauricio Carvajal
-CompleteResponses <- read.csv("C:\\Users\\lmaur\\Google Drive\\U\\DataAnalyticsBigData\\Data\\2_Predicting_Customer_Preferences\\2_Classification_Predict_which_Brand_of_Products_Customers_Prefer\\Data\\CompleteResponses.csv")
+#install Packges for c50
+#installed.packages("C50", dependencies = c("Depends", "Suggests"))
+
 #Libraries needed for the script.
-#install.packages("C50")
+
 library(caret) #caret-->clasificacion and regresion testing
 library(C50)
+CompleteResponses <- read.csv("Data\\CompleteResponses.csv")
+
+
 #Starting gettng knowleage of the data
 #attributes(CompleteResponses)
 summary(CompleteResponses)
@@ -24,8 +29,6 @@ CompleteResponses$brand<-as.factor(CompleteResponses$brand)
 #Checking the convertion
 str(CompleteResponses)
 
-
-
 #Seed needed for repeability 
 set.seed(123)
 inTrain <- createDataPartition(y = CompleteResponses$brand,
@@ -38,47 +41,70 @@ training <- CompleteResponses[ inTrain,]
 testing <- CompleteResponses[-inTrain,]
 
 #10 fold cross validation
-c50modelControl <- trainControl(method = "repeatedcv", number = 10, repeats = 1)
+c50modelControl <- trainControl(method = "repeatedcv", number = 10,repeats = 3)
 
 c50model <- train(brand~., 
                 data = training, 
                 method = "C5.0", 
-                trControl=fitControl, 
+                trControl=c50modelControl, 
                 tuneLength = 1,
-                preProc = c("center", "scale"))
+                preProc = c("center", "scale"),
+                verbose = TRUE)
 
 #training results
 c50model
+varImp(c50model, scale =FALSE)
+varImp(c50model)
 #train Linear Regression model with a tuneLenght = 2 (trains with 2 mtry values for RandomForest)
 c50model2 <- train(brand~., 
                   data = training, 
                   method = "C5.0", 
-                  trControl=fitControl, 
+                  trControl=c50modelControl, 
                   tuneLength = 2,
-                  preProc = c("center", "scale"))
+                  preProc = c("center", "scale"),
+                  verbose = TRUE)
 #training results
 c50model2
-varImp(c50model, scale =FALSE)
+varImp(c50model2, scale =FALSE)
 varImp(c50model2)
 
-#10 fold cross validation
-rfModelControl <- trainControl(method = "repeatedcv", number = 10, repeats = 1)
+rfmodelControl <- trainControl(method = "repeatedcv", number = 10,repeats = 3)
+rfGrid <- expand.grid(mtry=c(1,2,3,4,5))
 
-#train Random Forest Regression model with a tuneLenght = 1 (trains with 1 mtry value for RandomForest)
-rfModel <- train(brand~., 
-                data = training, 
-                method = "rf", 
-                trControl=fitControl, 
-                tuneLength = 1,
-                preProc = c("center", "scale"))
+rfmodel <- train(brand~., 
+                  data = training, 
+                  method = "rf", 
+                  trControl=rfmodelControl, 
+                  #tuneLength = 1,
+                  tuneGrid=rfGrid, 
+                  preProc = c("center", "scale"),
+                  verbose = TRUE)
 
 #training results
-rfModel
-varImp(rfModel)
-#train Linear Regression model with a tuneLenght = 11 (trains with 11 mtry values for RandomForest)
-rfModel11 <- train(brand~., 
-                 data = training, 
-                 method = "rf", 
-                 trControl=fitControl, 
-                 tuneLength = 11,
-                 preProc = c("center", "scale"))
+rfmodel
+varImp(rfmodel, scale =FALSE)
+varImp(rfmodel)
+################################################################
+#
+#                     Prediction part
+#
+################################################################
+SurveyIncomplete <- read.csv("Data\\CompleteResponses.csv")
+str(SurveyIncomplete)
+names(SurveyIncomplete)
+#hist(CompleteResponses$age)
+#plot(CompleteResponses$salary)
+#qnorm(CompleteResponses)
+
+#To check is.xxx -->True/Fase, to convert is as.xxx
+#is.numeric(CompleteResponses$salary)
+
+#Converting the numeric Values into categorical.
+SurveyIncomplete$elevel<-as.factor(CompleteResponses$elevel)
+SurveyIncomplete$car<-as.factor(CompleteResponses$car)
+SurveyIncomplete$zipcode<-as.factor(CompleteResponses$zipcode)
+SurveyIncomplete$brand<-as.factor(CompleteResponses$brand)
+#Checking the convertion
+str(SurveyIncomplete)
+
+
