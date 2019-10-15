6937d6213d0a73bc5e073b5c1ac13d6f30266fe7
lmauriciocarvajal <32909643+lmauriciocarvajal@users.noreply.github.com>
2019-10-14 18:07:09 -0600

    Final revision

--- 3_Deep_Analytics_and_Visualization/1_DomainResearch_ExploratoryDataAnalysis/DomainResearch_ExploratoryDataAnalysis/DomainResearch_ExploratoryDataAnalysis.R
+++ 3_Deep_Analytics_and_Visualization/1_DomainResearch_ExploratoryDataAnalysis/DomainResearch_ExploratoryDataAnalysis/DomainResearch_ExploratoryDataAnalysis.R
@@ -8,6 +8,7 @@
 #############################################################################################
 install.packages("RMySQL")
 install.packages("lubridate")
+install.packages("forcats")
 
 #############################################################################################
 # Libraries
@@ -15,6 +16,9 @@ install.packages("lubridate")
 library(RMySQL)
 library(dplyr)
 library(lubridate)
+library(ggplot2)
+library(forcats)
+library(RColorBrewer)
 
 #############################################################################################
 # SQL
@@ -143,42 +147,240 @@ unifyDataFrame$year <- year(unifyDataFrame$DateTime)
 unifyDataFrame$quarter <- quarter(unifyDataFrame$DateTime)
 unifyDataFrame$month <- month(unifyDataFrame$DateTime)
 unifyDataFrame$week <- week(unifyDataFrame$DateTime)
-unifyDataFrame$weekday <- weekday(unifyDataFrame$DateTime)
+unifyDataFrame$weekdays <- weekdays(unifyDataFrame$DateTime)
 unifyDataFrame$day <- day(unifyDataFrame$DateTime)
 unifyDataFrame$hour  <- hour(unifyDataFrame$DateTime)
 unifyDataFrame$minute <- minute(unifyDataFrame$DateTime)
 
 #############################################################################################
-# Initial Exploration of the Data
+# Creating a plot of power comsuption per quarter/year.
 #############################################################################################
-summary(unifyDataFrame)
-str(unifyDataFrame)
 
-#Creating new structure
-globalHeatMap <- unifyDataFrame[,c(5,12,14,15,16)]
-str(globalHeatMap)
+#Getting the average value per quarter, also mutipling 1000/60 as the documentation suggest.
+#Notes:
+#1.(global_active_power*1000/60 - sub_metering_1 - sub_metering_2 - sub_metering_3) represents the active energy consumed every minute (in watt hour) in the household by electrical equipment not measured in sub-meterings 1, 2 and 3.
+dataset2006q1  <-0*(1000/60)*(as.numeric(summarise(filter(unifyDataFrame, year==2006 & quarter ==1), delay = mean(Global_active_power, na.rm = TRUE))))
+dataset2006q2  <-0*(1000/60)*(as.numeric(summarise(filter(unifyDataFrame, year==2006 & quarter ==2), delay = mean(Global_active_power, na.rm = TRUE))))
+dataset2006q3  <-0*(1000/60)*(as.numeric(summarise(filter(unifyDataFrame, year==2006 & quarter ==3), delay = mean(Global_active_power, na.rm = TRUE))))
+dataset2006q4  <-(1000/60)*(as.numeric(summarise(filter(unifyDataFrame, year==2006 & quarter ==4), delay = mean(Global_active_power, na.rm = TRUE))))
+dataset2007q1  <-(1000/60)*(as.numeric(summarise(filter(unifyDataFrame, year==2007 & quarter ==1), delay = mean(Global_active_power, na.rm = TRUE))))
+dataset2007q2  <-(1000/60)*(as.numeric(summarise(filter(unifyDataFrame, year==2007 & quarter ==2), delay = mean(Global_active_power, na.rm = TRUE))))
+dataset2007q3  <-(1000/60)*(as.numeric(summarise(filter(unifyDataFrame, year==2007 & quarter ==3), delay = mean(Global_active_power, na.rm = TRUE))))
+dataset2007q4  <-(1000/60)*(as.numeric(summarise(filter(unifyDataFrame, year==2007 & quarter ==4), delay = mean(Global_active_power, na.rm = TRUE))))
+dataset2008q1  <-(1000/60)*(as.numeric(summarise(filter(unifyDataFrame, year==2008 & quarter ==1), delay = mean(Global_active_power, na.rm = TRUE))))
+dataset2008q2  <-(1000/60)*(as.numeric(summarise(filter(unifyDataFrame, year==2008 & quarter ==2), delay = mean(Global_active_power, na.rm = TRUE))))
+dataset2008q3  <-(1000/60)*(as.numeric(summarise(filter(unifyDataFrame, year==2008 & quarter ==3), delay = mean(Global_active_power, na.rm = TRUE))))
+dataset2008q4  <-(1000/60)*(as.numeric(summarise(filter(unifyDataFrame, year==2008 & quarter ==4), delay = mean(Global_active_power, na.rm = TRUE))))
+dataset2009q1  <-(1000/60)*(as.numeric(summarise(filter(unifyDataFrame, year==2009 & quarter ==1), delay = mean(Global_active_power, na.rm = TRUE))))
+dataset2009q2  <-(1000/60)*(as.numeric(summarise(filter(unifyDataFrame, year==2009 & quarter ==2), delay = mean(Global_active_power, na.rm = TRUE))))
+dataset2009q3  <-(1000/60)*(as.numeric(summarise(filter(unifyDataFrame, year==2009 & quarter ==3), delay = mean(Global_active_power, na.rm = TRUE))))
+dataset2009q4  <-(1000/60)*(as.numeric(summarise(filter(unifyDataFrame, year==2009 & quarter ==4), delay = mean(Global_active_power, na.rm = TRUE))))
+dataset2010q1  <-(1000/60)*(as.numeric(summarise(filter(unifyDataFrame, year==2010 & quarter ==1), delay = mean(Global_active_power, na.rm = TRUE))))
+dataset2010q2  <-(1000/60)*(as.numeric(summarise(filter(unifyDataFrame, year==2010 & quarter ==2), delay = mean(Global_active_power, na.rm = TRUE))))
+dataset2010q3  <-(1000/60)*(as.numeric(summarise(filter(unifyDataFrame, year==2010 & quarter ==3), delay = mean(Global_active_power, na.rm = TRUE))))
+dataset2010q4  <-(1000/60)*(as.numeric(summarise(filter(unifyDataFrame, year==2010 & quarter ==4), delay = mean(Global_active_power, na.rm = TRUE))))
 
-######## Plotting starts here#####################
-p <-ggplot(globalHeatMap,aes(day,hour,fill=Global_active_power))+
-  geom_tile(color= "white",size=0.1) + 
-  scale_fill_viridis(name="Hrly Temps C",option ="C")
-p <-p + facet_grid(year~month)
-p <-p + scale_y_continuous(trans = "reverse", breaks = unique(globalHeatMap$hour))
-p <-p + scale_x_continuous(breaks =c(1,10,20,31))
-p <-p + theme_minimal(base_size = 8)
-p <-p + labs(title= paste("Hourly Temps - Station"), x="Day", y="Hour Commencing")
-p <-p + theme(legend.position = "bottom")+
-  theme(plot.title=element_text(size = 14))+
-  theme(axis.text.y=element_text(size=6)) +
-  theme(strip.background = element_rect(colour="white"))+
-  theme(plot.title=element_text(hjust=0))+
-  theme(axis.ticks=element_blank())+
-  theme(axis.text=element_text(size=7))+
-  theme(legend.title=element_text(size=8))+
-  theme(legend.text=element_text(size=6))+
-  removeGrid()#ggExtra
+#Creating the matrix that will be used for plotting, 2006 will be deleted due to missing data, but will be analized in next graphics 
+yeardataall<-cbind(
+                   #c(dataset2006q1,dataset2006q2,dataset2006q3,dataset2006q4),
+                   c(dataset2007q1,dataset2007q2,dataset2007q3,dataset2007q4),
+                   c(dataset2008q1, dataset2008q2, dataset2008q3, dataset2008q4),
+                   c(dataset2009q1,dataset2009q2,dataset2009q3,dataset2009q4),
+                   c(dataset2010q1,dataset2010q2,dataset2010q3,dataset2010q4))
 
-# you will want to expand your plot screen before this bit!
-p #awesomeness
+#Renamming the cols and rows
+colnames(yeardataall) <- c("2007","2008","2009","2010")
+rownames(yeardataall) <- c("Q1","Q2","Q3","Q4")
+
+
+# Creating the plot
+coul <- brewer.pal(4, "Paired")
+barplot(yeardataall, 
+        col=coul ,
+        border="white", 
+        space=0.04, 
+        font.axis=2,
+        legend=rownames(yeardataall), 
+        xlab="Distribution of totalconsumption of power thru years",
+        ylab="Average power in Watts")
+
+
+#############################################################################################
+# Distribution of Total power thru the week days
+#############################################################################################
+#Getting the average power from 2006-2010 per weekdays
+datasetMonday   <-(1000/60)*(as.numeric(summarise(filter(unifyDataFrame, weekdays == "Monday"  ), delay = mean(Global_active_power, na.rm = TRUE))))
+datasetTuesday  <-(1000/60)*(as.numeric(summarise(filter(unifyDataFrame, weekdays =="Tuesday"  ), delay = mean(Global_active_power, na.rm = TRUE))))  
+datasetWednesday<-(1000/60)*(as.numeric(summarise(filter(unifyDataFrame, weekdays =="Wednesday"), delay = mean(Global_active_power, na.rm = TRUE))))  
+datasetThursday <-(1000/60)*(as.numeric(summarise(filter(unifyDataFrame, weekdays =="Thursday" ), delay = mean(Global_active_power, na.rm = TRUE))))  
+datasetFriday   <-(1000/60)*(as.numeric(summarise(filter(unifyDataFrame, weekdays =="Friday"   ), delay = mean(Global_active_power, na.rm = TRUE))))  
+datasetSaturday <-(1000/60)*(as.numeric(summarise(filter(unifyDataFrame, weekdays =="Saturday" ), delay = mean(Global_active_power, na.rm = TRUE))))  
+datasetSunday   <-(1000/60)*(as.numeric(summarise(filter(unifyDataFrame, weekdays =="Sunday"   ), delay = mean(Global_active_power, na.rm = TRUE))))
+# Create data
+
+Global_Active_Power_Bar_per_day <- data.frame(
+  name=c("Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday") ,  
+  val=c(datasetMonday,datasetTuesday,datasetWednesday,datasetThursday,datasetFriday,datasetSaturday,datasetSunday)
+)
+
+# Reorder following the value of another column:
+Global_Active_Power_Bar_per_day %>%
+  arrange(val) %>%
+  mutate(name = factor(name, levels=c("Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"))) %>%
+  ggplot( aes(x=name, y=val)) +
+  geom_segment( aes(xend=name, yend=0)) +
+  geom_point( size=6, color="dark green") +
+  theme_bw() +
+  xlab("Distribution of totalconsumption of power per days weeks")+
+  ylab("Average power in Watts")
+
+
+
+#############################################################################################
+# Summary of Distribution of Total power thru the week days per sub metering
+#############################################################################################
+#Getting the values of sub1
+sub1_datasetMonday   <-as.numeric(summarise(filter(unifyDataFrame, weekdays == "Monday"  ), delay = mean(Sub_metering_1, na.rm = TRUE)))
+sub1_datasetTuesday  <-as.numeric(summarise(filter(unifyDataFrame, weekdays =="Tuesday"  ), delay = mean(Sub_metering_1, na.rm = TRUE)))  
+sub1_datasetWednesday<-as.numeric(summarise(filter(unifyDataFrame, weekdays =="Wednesday"), delay = mean(Sub_metering_1, na.rm = TRUE)))  
+sub1_datasetThursday <-as.numeric(summarise(filter(unifyDataFrame, weekdays =="Thursday" ), delay = mean(Sub_metering_1, na.rm = TRUE)))  
+sub1_datasetFriday   <-as.numeric(summarise(filter(unifyDataFrame, weekdays =="Friday"   ), delay = mean(Sub_metering_1, na.rm = TRUE)))  
+sub1_datasetSaturday <-as.numeric(summarise(filter(unifyDataFrame, weekdays =="Saturday" ), delay = mean(Sub_metering_1, na.rm = TRUE)))  
+sub1_datasetSunday   <-as.numeric(summarise(filter(unifyDataFrame, weekdays =="Sunday"   ), delay = mean(Sub_metering_1, na.rm = TRUE)))
+#Getting the values of sub2
+sub2_datasetMonday   <-as.numeric(summarise(filter(unifyDataFrame, weekdays == "Monday"  ), delay = mean(Sub_metering_2, na.rm = TRUE)))
+sub2_datasetTuesday  <-as.numeric(summarise(filter(unifyDataFrame, weekdays =="Tuesday"  ), delay = mean(Sub_metering_2, na.rm = TRUE)))  
+sub2_datasetWednesday<-as.numeric(summarise(filter(unifyDataFrame, weekdays =="Wednesday"), delay = mean(Sub_metering_2, na.rm = TRUE)))  
+sub2_datasetThursday <-as.numeric(summarise(filter(unifyDataFrame, weekdays =="Thursday" ), delay = mean(Sub_metering_2, na.rm = TRUE)))  
+sub2_datasetFriday   <-as.numeric(summarise(filter(unifyDataFrame, weekdays =="Friday"   ), delay = mean(Sub_metering_2, na.rm = TRUE)))  
+sub2_datasetSaturday <-as.numeric(summarise(filter(unifyDataFrame, weekdays =="Saturday" ), delay = mean(Sub_metering_2, na.rm = TRUE)))  
+sub2_datasetSunday   <-as.numeric(summarise(filter(unifyDataFrame, weekdays =="Sunday"   ), delay = mean(Sub_metering_2, na.rm = TRUE)))
+#Getting the values of sub3
+sub3_datasetMonday   <-as.numeric(summarise(filter(unifyDataFrame, weekdays == "Monday"  ), delay = mean(Sub_metering_3, na.rm = TRUE)))
+sub3_datasetTuesday  <-as.numeric(summarise(filter(unifyDataFrame, weekdays =="Tuesday"  ), delay = mean(Sub_metering_3, na.rm = TRUE)))  
+sub3_datasetWednesday<-as.numeric(summarise(filter(unifyDataFrame, weekdays =="Wednesday"), delay = mean(Sub_metering_3, na.rm = TRUE)))  
+sub3_datasetThursday <-as.numeric(summarise(filter(unifyDataFrame, weekdays =="Thursday" ), delay = mean(Sub_metering_3, na.rm = TRUE)))  
+sub3_datasetFriday   <-as.numeric(summarise(filter(unifyDataFrame, weekdays =="Friday"   ), delay = mean(Sub_metering_3, na.rm = TRUE)))  
+sub3_datasetSaturday <-as.numeric(summarise(filter(unifyDataFrame, weekdays =="Saturday" ), delay = mean(Sub_metering_3, na.rm = TRUE)))  
+sub3_datasetSunday   <-as.numeric(summarise(filter(unifyDataFrame, weekdays =="Sunday"   ), delay = mean(Sub_metering_3, na.rm = TRUE)))
+#Getting the values of the rest of the house, using the especifications:
+#Notes:
+#1.(global_active_power*1000/60 - sub_metering_1 - sub_metering_2 - sub_metering_3) represents the active energy consumed every minute (in watt hour) in the household by electrical equipment not measured in sub-meterings 1, 2 and 3.
+notMeasureDatasetMonday   <-datasetMonday   -sub1_datasetMonday   -sub2_datasetMonday   -sub3_datasetMonday   
+notMeasureDatasetTuesday  <-datasetTuesday  -sub1_datasetTuesday  -sub2_datasetTuesday  -sub3_datasetTuesday  
+notMeasureDatasetWednesday<-datasetWednesday-sub1_datasetWednesday-sub2_datasetWednesday-sub3_datasetWednesday
+notMeasureDatasetThursday <-datasetThursday -sub1_datasetThursday -sub2_datasetThursday -sub3_datasetThursday 
+notMeasureDatasetFriday   <-datasetFriday   -sub1_datasetFriday   -sub2_datasetFriday   -sub3_datasetFriday   
+notMeasureDatasetSaturday <-datasetSaturday -sub1_datasetSaturday -sub2_datasetSaturday -sub3_datasetSaturday 
+notMeasureDatasetSunday   <-datasetSunday   -sub1_datasetSunday   -sub2_datasetSunday   -sub3_datasetSunday  
+#Creating the matrix
+submeterdataall<-rbind(c(sub1_datasetMonday  ,sub1_datasetTuesday  ,sub1_datasetWednesday,sub1_datasetThursday ,sub1_datasetFriday   ,sub1_datasetSaturday ,sub1_datasetSunday),
+                       c(sub2_datasetMonday  ,sub2_datasetTuesday  ,sub2_datasetWednesday,sub2_datasetThursday ,sub2_datasetFriday   ,sub2_datasetSaturday ,sub2_datasetSunday),
+                       c(sub3_datasetMonday  ,sub3_datasetTuesday  ,sub3_datasetWednesday,sub3_datasetThursday ,sub3_datasetFriday   ,sub3_datasetSaturday ,sub3_datasetSunday),
+                       c(notMeasureDatasetMonday,notMeasureDatasetTuesday,notMeasureDatasetWednesday,notMeasureDatasetThursday,notMeasureDatasetFriday,notMeasureDatasetSaturday,notMeasureDatasetSunday))
+#Renamming the cols and rows
+colnames(submeterdataall) <- c("Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday")
+rownames(submeterdataall) <- c("SubMetering1","SubMetering2","SubMetering3","NoMeasurebySub")
+coul <- brewer.pal(4, "Paired")
+# Creating the plot
+barplot(submeterdataall, 
+        col=coul ,
+        #col=colors()[c(23,89,12,60)] , 
+        border="white", 
+        font.axis=2, 
+        beside=T, 
+        legend=rownames(submeterdataall), 
+        xlab="Distribution of totalconsumption of power per days weeks",
+        ylab="Average power in Watts",
+        font.lab=2)
+
+#############################################################################################
+# Porcentage of Distribution of Total power thru the week days per sub metering
+#############################################################################################
+# Transform the data in Porcentage
+data_percentage <- apply(submeterdataall, 2, function(x){x*100/sum(x,na.rm=T)})
+# Make a stacked barplot
+barplot(data_percentage, 
+        col=coul ,
+        legend=rownames(submeterdataall), 
+        border="white", 
+        xlab="Distribution of totalconsumption of power per days weeks",
+        ylab="Porcentage %")
+
+#############################################################################################
+# Creating the individual plots per sub metering 
+#############################################################################################
+#SubMeter1
+SubMetering1<-rbind(c(sub1_datasetMonday  ,sub1_datasetTuesday  ,sub1_datasetWednesday,sub1_datasetThursday ,sub1_datasetFriday   ,sub1_datasetSaturday ,sub1_datasetSunday))
+colnames(SubMetering1) <- c("Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday")
+rownames(SubMetering1) <- c("SubMetering1")
+barplot(SubMetering1, 
+        border="white",
+        col="dark green" ,
+        font.axis=2, 
+        beside=T, 
+        #legend=rownames(submeterdataall), 
+        xlab="Distribution of totalconsumption of power per days weeks",
+        ylab="Average power in Watts",
+        main="Sub-Meter 1",
+        font.lab=2)
+#############################################################################################
+# Creating the individual plots per sub metering 
+#############################################################################################
+#SubMeter 2
+SubMetering2<-rbind(c(sub2_datasetMonday  ,sub2_datasetTuesday  ,sub2_datasetWednesday,sub2_datasetThursday ,sub2_datasetFriday   ,sub2_datasetSaturday ,sub2_datasetSunday))
+colnames(SubMetering2) <- c("Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday")
+rownames(SubMetering2) <- c("SubMetering2")
+barplot(SubMetering2, 
+        border="white",
+        col="dark green" ,
+        font.axis=2, 
+        beside=T, 
+        #legend=rownames(submeterdataall), 
+        xlab="Distribution of totalconsumption of power per days weeks",
+        ylab="Average power in Watts",
+        main="Sub-Meter 2",
+        font.lab=2)
+#############################################################################################
+# Creating the individual plots per sub metering 
+#############################################################################################
+#SubMeter 3
+SubMetering3<-rbind(c(sub3_datasetMonday  ,sub3_datasetTuesday  ,sub3_datasetWednesday,sub3_datasetThursday ,sub3_datasetFriday   ,sub3_datasetSaturday ,sub3_datasetSunday))
+colnames(SubMetering3) <- c("Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday")
+rownames(SubMetering3) <- c("SubMetering3")
+barplot(SubMetering3, 
+        border="white", 
+        col="dark green" ,
+        font.axis=2, 
+        beside=T, 
+        #legend=rownames(submeterdataall), 
+        xlab="Distribution of totalconsumption of power per days weeks",
+        ylab="Average power in Watts",
+        main="Sub-Meter 3",
+        font.lab=2)
+
+#############################################################################################
+# Creating the individual plots per sub metering 
+#############################################################################################
+#No Measure by sub meter
+NoMeasurebySub<-rbind(c(notMeasureDatasetMonday,notMeasureDatasetTuesday,notMeasureDatasetWednesday,notMeasureDatasetThursday,notMeasureDatasetFriday,notMeasureDatasetSaturday,notMeasureDatasetSunday))
+colnames(NoMeasurebySub) <- c("Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday")
+rownames(NoMeasurebySub) <- c("NoMeasurebySub")
+barplot(NoMeasurebySub, 
+        col="dark green",
+        border="white", 
+        font.axis=2, 
+        beside=T, 
+        #legend=rownames(NoMeasurebySub), 
+        xlab="Distribution of totalconsumption of power per days weeks",
+        ylab="Average power in Watts",
+        main="No Measure by any sub meter",
+        font.lab=2)
+
+
+
+
+
+
 
 
